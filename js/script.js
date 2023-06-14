48b6063f24a861fa1a77e51f7411b093a3c02e91
seanorourke7 <>
2023-06-14 16:36:32 +0000

    link quiz, scores and rules page, added rules content

--- js/script.js
+++
@@ -1,8 +0,0 @@
-let enterQuiz = document.getElementById("enter-quiz");
-enterQuiz.addEventListener('click', startQuestion());
-
-function startQuestion() {
-    let question = getElementById("question-display");
-    question.innerText = "Question 1";
-
-}
